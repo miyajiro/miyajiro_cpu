a7c2a9d4846b14811b106993da4997f2463b0ce1
miyajiro <ryosuke00719@gmail.com>
2023-01-01 02:04:40 +0900

    implement ROM.

---
+++ rom.v
@@ -0,0 +1,26 @@
+`include "include/define.v"
+
+module ROM (
+    input clk,
+    input reset_n,
+    input [`ROM_ADDRESS_BITWIDTH - 1:0] address,
+    output [31:0] data,
+);
+
+reg [`ROM_ADDRESS_BITWIDTH - 3:0] _address;
+reg [31:0] _rom [`ROM_SIZE / 4 - 1:0];
+
+initial $readmemh("fibonacci.mem", _rom);
+
+always @(posedge clk) begin
+    if(!reset_n) begin
+        _address <= 0;
+    end
+    else begin
+        _address <= address[`ROM_ADDRESS_BITWIDTH - 1 : 2];
+    end
+end
+
+assign rom_data = _rom[_address]
+
+endmodule
