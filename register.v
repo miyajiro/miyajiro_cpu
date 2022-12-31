df11564db44f7e4a83970ecfb5589a05d09abeb6
miyajiro <ryosuke00719@gmail.com>
2023-01-01 02:05:03 +0900

    implement REGISTER.

---
+++ register.v
@@ -0,0 +1,19 @@
+module REGISTER (
+    input reset_n,
+    input clk,
+    input write,
+    input [31:0] in_data,
+    output reg [31:0] data
+);
+
+always @(posedge clk) begin
+    if(!reset_n) begin
+        data <= 32'b0;
+    end
+    else begin
+        if(write) begin
+            data <= in_data
+        end
+    end
+end
+endmodule
