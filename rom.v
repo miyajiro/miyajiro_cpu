d477299411370ba3285b20c1b9453f513036d528
miyajiro <ryosuke00719@gmail.com>
2023-01-04 00:32:56 +0900

    set output.

--- rom.v
+++ rom.v
@@ -4,7 +4,7 @@ module ROM (
     input clk,
     input reset_n,
     input [`ROM_ADDRESS_BITWIDTH - 1:0] address,
-    output [31:0] data
+    output reg [31:0] data
 );
 
 reg [`ROM_ADDRESS_BITWIDTH - 3:0] _address;
@@ -21,6 +21,11 @@ always @(posedge clk) begin
     end
 end
 
-assign rom_data = _rom[_address];
+wire [31:0] _data;
+assign _data = _rom[_address];
+
+always @* begin
+    data <= _data;
+end
 
 endmodule
