fcefc590c5880890461c66cdfca58be2dc8a8d52
miyajiro <ryosuke00719@gmail.com>
2023-01-03 18:19:15 +0900

    fix input file name.

--- rom.v
+++ rom.v
@@ -10,7 +10,7 @@ module ROM (
 reg [`ROM_ADDRESS_BITWIDTH - 3:0] _address;
 reg [31:0] _rom [`ROM_SIZE / 4 - 1:0];
 
-initial $readmemh("gcd.bin", _rom);
+initial $readmemh("test/gcd.bin", _rom);
 
 always @(posedge clk) begin
     if(!reset_n) begin
