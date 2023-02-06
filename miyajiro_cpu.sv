da8112443766051c8cc42b0f16733303fd5e358a
miyajiro <ryosuke00719@gmail.com>
2023-02-07 03:36:10 +0900

    rename rom to program_memory.

--- miyajiro_cpu.sv
+++ miyajiro_cpu.sv
@@ -44,12 +44,12 @@ WB_IF_PIPELINE_REGISTER wb_if_pipeline_register(
 );
 
 // IF
-logic [31:0] if_rom_data;
-ROM rom(
+logic [31:0] if_program_memory_data;
+PROGRAM_MEMORY program_memory(
     .clk(clk),
     .reset_n(reset_n),
-    .address(if_pc_data[`ROM_ADDRESS_BITWIDTH - 1:0]),
-    .data(if_rom_data)
+    .address(if_pc_data[`PROGRAM_MEMORY_ADDRESS_BITWIDTH - 1:0]),
+    .data(if_program_memory_data)
 );
 
 // IF -> ID
@@ -59,7 +59,7 @@ IF_ID_PIPELINE_REGISTER if_id_pipeline_register(
     .reset_n(stage_controller_stage_reset_n),
     .clk(clk),
     .wren(stage_controller_if_id_wren),
-    .in_instruction(if_rom_data),
+    .in_instruction(if_program_memory_data),
     .in_pc_data(if_pc_data),
     .instruction(id_instruction_data),
     .pc_data(id_pc_data)
--- program_memory.sv
+++ program_memory.sv
@@ -1,29 +1,29 @@
 `timescale 1ns / 1ps
 `include "define.sv"
 
-module ROM (
+module PROGRAM_MEMORY (
     input logic clk,
     input logic reset_n,
-    input logic [`ROM_ADDRESS_BITWIDTH - 1:0] address,
+    input logic [`PROGRAM_MEMORY_ADDRESS_BITWIDTH - 1:0] address,
     output logic [31:0] data
 );
 
-logic [`ROM_ADDRESS_BITWIDTH - 3:0] _address;
-(* ram_style = "BLOCK" *) reg [31:0] _rom [`ROM_SIZE / 4 - 1:0];
+logic [`PROGRAM_MEMORY_ADDRESS_BITWIDTH - 3:0] _address;
+(* ram_style = "BLOCK" *) reg [31:0] _program_memory [`PROGRAM_MEMORY_SIZE / 4 - 1:0];
 
-initial $readmemb("program.dat", _rom);
+initial $readmemb("program.dat", _program_memory);
 
 always_ff @(posedge clk) begin
     if(!reset_n) begin
         _address <= 0;
     end
     else begin
-        _address <= address[`ROM_ADDRESS_BITWIDTH - 1 : 2];
+        _address <= address[`PROGRAM_MEMORY_ADDRESS_BITWIDTH - 1 : 2];
     end
 end
 
 always_comb begin
-    data <= _rom[_address];
+    data <= _program_memory[_address];
 end
 
 endmodule
