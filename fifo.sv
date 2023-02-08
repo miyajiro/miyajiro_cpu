9d8db53843742930737586e4ea9f323c0a1964ea
miyajiro <ryosuke00719@gmail.com>
2023-02-08 20:18:17 +0900

    fix fifo.

--- fifo.sv
+++ fifo.sv
@@ -11,9 +11,8 @@ module FIFO(
     output logic write_ready
 );
 
-logic [`FIFO_CAPACITY_BITWIDTH:0] read_address;
-logic [`FIFO_CAPACITY_BITWIDTH:0] write_address;
-logic [`FIFO_CAPACITY_BITWIDTH:0] address_difference;
+logic [`FIFO_CAPACITY_BITWIDTH:0] read_position;
+logic [`FIFO_CAPACITY_BITWIDTH:0] write_position;
 logic fifo_full;
 logic fifo_empty;
 
@@ -21,25 +20,36 @@ logic fifo_empty;
 
 always_ff @(posedge clk) begin
     if (~reset_n) begin
-        read_address <= 0;
+        read_position <= 0;
     end
     else if (read_enable) begin
-        read_address <= read_address + 1;
+        read_position <= read_position + 1;
     end
 end
 
 always_ff @(posedge clk) begin
     if (~reset_n) begin
-        write_address <= 0;
+        write_position <= 0;
     end
     else if (write_enable) begin
-        write_address <= write_address + 1;
+        write_position <= write_position + 1;
     end
 end
 
+logic [`FIFO_CAPACITY_BITWIDTH - 1:0] read_address;
+logic read_position_prefix;
+logic [`FIFO_CAPACITY_BITWIDTH - 1:0] write_address;
+logic write_position_prefix;
+
+assign read_address = read_position[`FIFO_CAPACITY_BITWIDTH - 1:0];
+assign read_position_prefix = read_position[`FIFO_CAPACITY_BITWIDTH];
+
+assign write_address = write_position[`FIFO_CAPACITY_BITWIDTH - 1:0];
+assign write_position_prefix = read_position[`FIFO_CAPACITY_BITWIDTH];
+
 always_comb begin
-    fifo_empty <= (read_address == write_address);
-    fifo_full <= (read_address[`FIFO_CAPACITY_BITWIDTH - 1:0] == write_address[`FIFO_CAPACITY_BITWIDTH - 1:0]) & (read_address[`FIFO_CAPACITY_BITWIDTH] != write_address[`FIFO_CAPACITY_BITWIDTH]);
+    fifo_empty <= (read_address == write_address) & (read_position_prefix == write_position_prefix);
+    fifo_full <= (read_address == write_address) & (read_position_prefix != write_position_prefix);
 end
 
 always_comb begin
@@ -49,11 +59,11 @@ end
 
 always_ff @(posedge clk) begin
     if (write_enable) begin
-        memory_array[write_address[`FIFO_CAPACITY_BITWIDTH - 1:0]] <= write_data;
+        memory_array[write_address] <= write_data;
     end
 end
 
 always_ff @(posedge clk) begin
-    read_data <= memory_array[read_address[`FIFO_CAPACITY_BITWIDTH - 1:0]];
+    read_data <= memory_array[read_address];
 end
 endmodule
