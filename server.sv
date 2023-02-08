76ad414ce8b9dfdc38f7e3aa8b293581709b97d6
miyajiro <ryosuke00719@gmail.com>
2023-02-09 02:52:56 +0900

    new gcd

--- server.sv
+++ server.sv
@@ -4,9 +4,10 @@
 module server(
 );
 
-localparam PROGRAM_DATA_SIZE_BYTE = 128;
+localparam PROGRAM_DATA_SIZE_BYTE = 256;
 localparam STDIN_DATA_SIZE_BYTE = 32;
 localparam RESULT_MEMORY_SIZE_BYTE = 128;
+
 localparam STATE_WAIT_0x99 = 3'h0;
 localparam STATE_PROGRAM_DATA_SIZE_SEND = 3'h1;
 localparam STATE_PROGRAM_DATA_SEND = 3'h2;
