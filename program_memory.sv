42908afa92deee36b0f54fb5cf76c9b914457aa8
miyajiro <ryosuke00719@gmail.com>
2023-02-08 23:00:55 +0900

    read_address.

--- program_memory.sv
+++ program_memory.sv
@@ -4,26 +4,26 @@
 module PROGRAM_MEMORY (
     input logic clk,
     input logic reset_n,
-    input logic [`PROGRAM_MEMORY_ADDRESS_BITWIDTH - 1:0] address,
+    input logic [`PROGRAM_MEMORY_ADDRESS_BITWIDTH - 1:0] read_address,
     output logic [31:0] data
 );
 
-logic [`PROGRAM_MEMORY_ADDRESS_BITWIDTH - 3:0] _address;
+logic [`PROGRAM_MEMORY_ADDRESS_BITWIDTH - 3:0] _read_address;
 (* ram_style = "BLOCK" *) reg [31:0] _program_memory [`PROGRAM_MEMORY_SIZE_BYTE / 4 - 1:0];
 
 // initial $readmemb("program.dat", _program_memory);
 
 always_ff @(posedge clk) begin
     if(!reset_n) begin
-        _address <= 0;
+        _read_address <= 0;
     end
     else begin
-        _address <= address[`PROGRAM_MEMORY_ADDRESS_BITWIDTH - 1 : 2];
+        _read_address <= read_address[`PROGRAM_MEMORY_ADDRESS_BITWIDTH - 1 : 2];
     end
 end
 
 always_comb begin
-    data <= _program_memory[_address];
+    data <= _program_memory[_read_address];
 end
 
 endmodule
