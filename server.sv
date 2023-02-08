195d9f7a59570e0a040d02534ff74c4ca8210e25
miyajiro <ryosuke00719@gmail.com>
2023-02-08 15:39:33 +0900

    server.sv

--- server.sv
+++ server.sv
@@ -4,9 +4,15 @@
 module server(
 );
 
-localparam program_data_size_byte = 96;
-localparam stdin_data_size_byte = 128;
-localparam result_memory_size_byte = 128;
+localparam PROGRAM_DATA_SIZE_BYTE = 96;
+localparam STDIN_DATA_SIZE_BYTE = 128;
+localparam RESULT_MEMORY_SIZE_BYTE = 128;
+localparam STATE_WAIT_0x99 = 3'h0;
+localparam STATE_PROGRAM_SIZE_SEND = 3'h1;
+localparam STATE_PROGRAM_DATA_SEND = 3'h2;
+localparam STATE_WAIT_0xAA = 3'h2;
+localparam STATE_STDIN_DATA_SEND = 3'h3;
+localparam STATE_WAIT_PPM  = 3'h4;
 
 logic clk;
 logic reset_n;
@@ -45,10 +51,11 @@ always begin
     # 5;
 end
 
+logic [31:0] program_data_size_byte;
 logic [2:0] program_data_size_section;
-logic [7:0] program_data [program_data_size_byte - 1:0];
+logic [7:0] program_data [PROGRAM_DATA_SIZE_BYTE - 1:0];
 logic [16:0] program_data_index;
-logic [7:0] stdin_data [stdin_data_size_byte:0];
+logic [7:0] stdin_data [STDIN_DATA_SIZE_BYTE:0];
 logic [16:0] stdin_data_index;
 
 initial begin
@@ -61,24 +68,19 @@ initial begin
     $finish();
 end
 
-logic [7:0] result_memory [result_memory_size_byte:0];
+logic [7:0] result_memory [RESULT_MEMORY_SIZE_BYTE:0];
 logic [16:0] result_memory_index;
 logic [2:0] state;
 logic busy_wait;
-localparam state_wait_0x99 = 3'h0;
-localparam state_program_size_send = 3'h1;
-localparam state_program_data_send = 3'h2;
-localparam state_wait_0xAA = 3'h2;
-localparam state_stdin_data_send = 3'h3;
-localparam state_wait_ppm  = 3'h4;
 
 always_ff @(posedge clk) begin
     if (~reset_n) begin
         tx_start <= 0;
+        program_data_size_byte <= PROGRAM_DATA_SIZE_BYTE;
         program_data_index <= 0;
         stdin_data_index <= 0;
         result_memory_index <= 0;
-        state <= state_wait_0x99;
+        state <= STATE_WAIT_0x99;
         busy_wait <= 0;
     end
     if (tx_start) begin
@@ -86,58 +88,59 @@ always_ff @(posedge clk) begin
     end
 
     case (state)
-        state_wait_0x99: begin
+        STATE_WAIT_0x99: begin
             if (server_uart_rx_rdata_ready && server_uart_rx_rdata == 8'h99) begin
-                state <= state_program_data_send;
+                state <= STATE_PROGRAM_DATA_SEND;
             end
         end
-        state_program_size_send: begin
+        STATE_PROGRAM_SIZE_SEND: begin
             if (~server_uart_tx_tx_busy && ~busy_wait) begin
                 if(program_data_size_section < 4) begin
-                    sdata <= program_data_size_byte[program_data_size_section * 4 + 3:program_data_size_section * 4];
+                    sdata <= program_data_size_byte[7:0];
+                    program_data_size_byte <= (program_data_size_byte >> 8);
                     tx_start <= 1;
                     program_data_size_section <= program_data_size_section + 1;
                     busy_wait <= 1;
                 end
                 else begin
-                    state <= state_stdin_data_send;
+                    state <= STATE_STDIN_DATA_SEND;
                 end
             end
             if (busy_wait) begin
                 busy_wait <= 0;
             end
         end
-        state_program_data_send: begin
+        STATE_PROGRAM_DATA_SEND: begin
             if (~server_uart_tx_tx_busy && ~busy_wait) begin
-                if (program_data_index < program_data_size_byte) begin
+                if (program_data_index < PROGRAM_DATA_SIZE_BYTE) begin
                     sdata <= program_data[program_data_index];
                     tx_start <= 1;
                     program_data_index <= program_data_index + 1;
                     busy_wait <= 1;
                 end
                 else begin
-                    state <= state_wait_0xAA;
+                    state <= STATE_WAIT_0xAA;
                 end
             end
             if (busy_wait) begin
                 busy_wait <= 0;
             end
         end
-        state_wait_0xAA: begin
+        STATE_WAIT_0xAA: begin
             if (server_uart_rx_rdata_ready && server_uart_rx_rdata == 8'hAA) begin
-                state <= state_stdin_data_send;
+                state <= STATE_STDIN_DATA_SEND;
             end
         end
-        state_stdin_data_send: begin
+        STATE_STDIN_DATA_SEND: begin
             if (~server_uart_tx_tx_busy && ~busy_wait) begin
-                if (stdin_data_index < stdin_data_size_byte) begin
+                if (stdin_data_index < STDIN_DATA_SIZE_BYTE) begin
                     sdata <= stdin_data[stdin_data_index];
                     tx_start <= 1;
                     stdin_data_index <= stdin_data_index + 1;
                     busy_wait <= 1;
                 end
                 else begin
-                    state <= state_wait_ppm;
+                    state <= STATE_WAIT_PPM;
                 end
             end
             if (busy_wait) begin
@@ -148,7 +151,7 @@ always_ff @(posedge clk) begin
                 result_memory_index <= result_memory_index + 1;
             end
         end
-        state_wait_ppm: begin
+        STATE_WAIT_PPM: begin
             if (server_uart_rx_rdata_ready) begin
                 result_memory[result_memory_index] <= server_uart_rx_rdata;
                 result_memory_index <= result_memory_index + 1;
