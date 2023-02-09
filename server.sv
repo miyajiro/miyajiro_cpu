2139c12f16214a71855a6a65545cb403ebe9319a
miyajiro <ryosuke00719@gmail.com>
2023-02-09 18:53:08 +0900

    flag.

--- server.sv
+++ server.sv
@@ -4,8 +4,8 @@
 module server(
 );
 
-localparam PROGRAM_DATA_SIZE_BYTE = 256; // gcd
-// localparam PROGRAM_DATA_SIZE_BYTE = 146 * 4; flag
+// localparam PROGRAM_DATA_SIZE_BYTE = 64 * 4; // gcd
+localparam PROGRAM_DATA_SIZE_BYTE = 146 * 4; flag
 
 localparam STDIN_DATA_SIZE_BYTE = 32;
 localparam RESULT_MEMORY_SIZE_BYTE = 1048576; // large
