09c8c839869fffac4d285082b492e907fc2d3eea
miyajiro <ryosuke00719@gmail.com>
2023-02-10 17:17:52 +0900

    wrapper.

--- miyajiro_cpu_wrapper.v
+++ miyajiro_cpu_wrapper.v
@@ -1,15 +1,17 @@
+`include "miyajiro_cpu.sv"
+
 module MIYAJIRO_CPU_WRAPPER (
-    input logic reset_n,
-    input logic clk,
-    input logic cpu_uart_rxd,
-    output logic cpu_uart_txd
-);
+    input reset_n,
+    input clk,
+    input cpu_uart_rxd,
+    output wire cpu_uart_txd
+)
 
 MIYAJIRO_CPU miyajiro_cpu(
     .reset_n(reset_n),
     .clk(reset_n),
     .cpu_uart_rxd(cpu_uart_rxd),
     .cpu_uart_txd(cpu_uart_txd)
-);
+)
 
 endmodule
